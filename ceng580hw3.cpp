db957747a9ce1b2ab334ed9c22f87911e603ad62
mchtalkn <mucahit.a.yorganci@gmail.com>
2022-06-16 15:29:51 +0300

    Add project files.

---
+++ ceng580hw3.cpp
@@ -0,0 +1,12 @@
+﻿// ceng580hw3.cpp : Defines the entry point for the application.
+//
+
+#include "ceng580hw3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
